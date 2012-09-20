c224facddf2456891b53610c8dd0edd5d5f343ad
Tsung-Yin Tsai <ttsaia@gmail.com>
2012-09-20 15:17:12 -0700

    Symfony first commit

---
+++ src/FbHack/BeerBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FbHack\BeerBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('FbHackBeerBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/FbHack/BeerBundle/FbHackBeerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FbHack\BeerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FbHackBeerBundle extends Bundle
+{
+}
